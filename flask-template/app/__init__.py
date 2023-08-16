b2ca68aabe2aaf84092e720c344944cc4bb0b02d
Luis Toribio <luistoribio@Luiss-MacBook-Pro.local>
2023-08-16 02:53:21 -0500

    add the cloudant and mongo models

--- flask-template/app/__init__.py
+++ flask-template/app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from app.routes.ocr.controller import ocr_blueprint as ocr_class
+from app.routes.manage_people.contoller import people_blueprint
 from dotenv import dotenv_values,load_dotenv
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(ocr_class)
-
+app.register_blueprint(people_blueprint)
 
 #declarar variable de entorno
 ENV = dotenv_values(".env")
--- flask-template/app/db/mongodb.py
+++ flask-template/app/db/mongodb.py
@@ -7,34 +7,52 @@ class MongoDB:
 
     def connection_database(self):
         #client = MongoClient(os.environ["MONGO_CONNECTION"])
-        URI = "mongodb://0.0.0.0:27728"
+        URI = os.environ["MONGO_CONNECTION"]
         client = MongoClient(URI)
-        print("client: ", client)
         return client
 
-    def create_data_collection(self):
+    def create_switch_data_collection(self):
         client = self.connection_database()
         #Creating a collection
         db = client['mydb']
         mongo_collection = db['alumnos']
-        #Inserting document into a collection
-        doc1 = {"name": "Jose L", "age": "31"}
-        mongo_collection.insert_one(doc1)
         
-        self.close_database(client)
-        
-        print("Collection alumnos created")
+        return mongo_collection,client
     
-    def find_one_data_value(self):
-        client = self.connection_database()
-        db = client['mydb']
-        mongo_collection =  db['alumnos']
+    
+    def insert_one_value(self,name,age):
+        mongo_collection,client = self.create_switch_data_collection()
+        doc1 = {"name": name, "age": age}
+        #Inserting document into a collection
+        mongo_collection.insert_one(doc1)
+        self.close_database(client)
+    
+    def find_one_data_value(self,column,value):
+        mongo_collection,client = self.create_switch_data_collection()
+        one_value = mongo_collection.find_one({column:value})
+        self.close_database(client)
         
-        one_value = mongo_collection.find_one()
+        return one_value
+
+        
+    def find_all_values(self):
+        list_values = []
+        mongo_collection,client = self.create_switch_data_collection()
+        values =  mongo_collection.find()
+        for value in values:
+            list_values.append(value)
         
         self.close_database(client)
         
-        print("Value",one_value)
+        return list_values
+    
+    def delete_one_value(self):
+        mongo_collection,client = self.create_switch_data_collection()
+        result_delete = mongo_collection.delete_one({"name":"Mr.Coder"})
+        print(result_delete)
+        self.close_database(client)
+        
+        return result_delete
         
     
     def close_database(self, mongo_client):
@@ -44,6 +62,7 @@ if __name__ == "__main__":
     obj_mongo = MongoDB()
     
     #create a collection 
-    obj_mongo.create_data_collection()
-    obj_mongo.find_one_data_value()
-    
+    obj_mongo.create_switch_data_collection()
+    #obj_mongo.find_one_data_value("name","Mariana L")
+    #obj_mongo.insert_one_value("Elizabeth Garcia",25)
+    #obj_mongo.find_all_values()
